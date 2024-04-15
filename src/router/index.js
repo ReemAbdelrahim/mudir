9c1fce60118d582e6e28b757b4ee1978b5bbd101
unknown <reemgadelrab99@gmail.com>
2024-04-15 18:00:13 +0200

    Ismail

--- src/router/index.js
+++ src/router/index.js
@@ -1,45 +1,45 @@
-import {createRouter , createWebHistory} from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes: [
-      {
-        path: "/",
-        name: "Login",
-        component: () => import("../Views/login/Login.vue")
-      },
-      {
-        path: "/home",
-        name: "Home",
-        component: () => import("../Views/home/Home.vue")
-      },      
-      {
-        path: "/myads",
-        name: "MyAds",
-        component: () => import("../Views/myads/myads.vue")
-      },
-      {
-        path: "/myads/:id",
-        name: "adDetails",
-        component: () => import("../Views/myads/adDetails.vue")
-      },
-      {
-        path: "/myads/add",
-        name: "AddAdv",
-        component: () => import("../Views/ads/ads.vue")
-      }
-      , {
-        path: "/chats",
-        name: "Chats",
-        component: () => import("../Views/chats/Chat.vue")
-      }
-      , {
-        path: "/stateMnagment",
-        name: "StateMnagment",
-        component: () => import("../Views/stateManagment/stateManagment.vue")
-      }
-      
-    ]
-  })
+  history: createWebHistory(),
+  routes: [
+    {
+      path: "/myads",
+      name: "MyAds",
+      component: () => import("../Views/myads/myads.vue")
+    },
+    {
+      path: "/myads/:id",
+      name: "adDetails",
+      component: () => import("../Views/myads/adDetails.vue")
+    },
+    {
+      path: "/myads/add",
+      name: "AddAdv",
+      component: () => import("../Views/ads/ads.vue")
+    },
+    {
+      path: "/chats",
+      name: "Chats",
+      component: () => import("../Views/chats/Chat.vue")
+    },
+    {
+      path: "/stateMnagment",
+      name: "StateMnagment",
+      component: () => import("../Views/stateManagment/stateManagment.vue")
+    },
+    {
+      path: "/",
+      name: "Login",
+      component: () => import("../Views/login/login.vue")
+    },
+    {
+      path: "/home",
+      name: "Home",
+      component: () => import("../Views/home/Home.vue")
+    },
+
+  ]
+})
 
 export default router
